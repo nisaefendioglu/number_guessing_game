eac88d76d1085d4cefa213f44785d643a1db12b7
Nisa <48391281+nisaefendioglu@users.noreply.github.com>
2019-09-12 14:33:24 +0300

    fix commit

--- src/number_guessing_game/Number_guessing_game.java
+++ src/number_guessing_game/Number_guessing_game.java
@@ -12,15 +12,16 @@ import java.util.Scanner;
  *
  * @author NisaEfendioglu
  */
+    
 public class Number_guessing_game {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-       Scanner input = new Scanner(System.in);
+    public static void number_guessing(){
+    
+    Scanner input = new Scanner(System.in);
        Random rnd = new Random();
-       System.out.println("Welcome!");
        System.out.print("Enter a number between 1-100.");
         int guess = input.nextInt();
         int number = rnd.nextInt(101);
@@ -37,7 +38,25 @@ public class Number_guessing_game {
         guess = input.nextInt();}
     }
 
-       System.out.println("Congrulations!"); }
+       System.out.println("Congrulations!"); 
+       again();
+}
+    public static void again() {
+    Scanner input=new Scanner(System.in);
+     System.out.print("Do you want to play again? YES or NO");
+       String answer=input.next();
+       if(answer.equalsIgnoreCase("YES")){
+           number_guessing();
+       }
+       else {
+           System.out.print("Have a nice day!");
+       }
+}
+    public static void main(String[] args) {
+        System.out.println("Welcome!");
+        number_guessing();
+      
+    }
 }
         // TODO code application logic here
     
